90ac1309d7d43dff671bc9f542b0c179008ab54d
Nguyễn Kim Ngọc <177725608+TumRoyall@users.noreply.github.com>
2026-06-12 02:45:19 +0700

    config app
    config app

--- Development/app_moneytracker/src/core/config/env.ts
+++ Development/app_moneytracker/src/core/config/env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
- apiBaseUrl: 'http://192.168.2.247:8080',
-  // apiBaseUrl: 'http://192.168.1.80:8080',
+  // Production backend on Railway
+  apiBaseUrl: 'https://moneytrackerapp.up.railway.app',
   appName: 'MoneyTracker',
-  webAppUrl: 'http://localhost:8081', // Sửa đường dẫn này thành Domain thực tế sau khi deploy Web (VD: https://moneytracker.com)
+  webAppUrl: 'http://localhost:8081', // TODO: update after deploying web admin
 };
